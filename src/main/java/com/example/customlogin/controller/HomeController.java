404b1c13e521cf462d750eeceeb550bc8ba01d2b
KaczmarekIgor <Igor009@interia.eu>
2020-04-24 08:50:20 +0200

    add home.html and HomeController is done

--- src/main/java/com/example/customlogin/controller/HomeController.java
+++ src/main/java/com/example/customlogin/controller/HomeController.java
@@ -1,9 +1,10 @@
 package com.example.customlogin.controller;
 
 
-import com.example.customlogin.model.Quiz;
+import com.example.customlogin.form.QuizForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 import java.util.ArrayList;
@@ -11,20 +12,23 @@ import java.util.List;
 
 @Controller
 public class HomeController {
-    private List<Quiz> quiz;
+
+
+    private List<QuizForm> quiz;
 
     public HomeController() {
         this.quiz = new ArrayList<>();
-        quiz.add(new Quiz("Sport","Takie koptyo. Kto uzyl takich slow, w odcinku Hejt-Park prowadzonym przez Mateusza Borka","?????"));
-        quiz.add(new Quiz("Kulinaria","Wojciech Stawowy czy Wojciech Schabowy kto prowadzil miedzy innymi Arke i Cracovie?","??????"));
-        quiz.add(new Quiz("Savoir-Vivre","Ja to sie nie wpierdalam czy nie wpraszam? Jak swoja szkoelnowia taktyke opisywal Pawel Janas","?????"));
-        quiz.add(new Quiz("Jezyk Polski","Franciszek Smuda to trener skuteczny. Zapowiedzial walke o spadek i rzeczywiscie spadl. Z jakim klubem?","?????"));
+        quiz.add(new QuizForm("Kulinaria","Wojciech Stawowy czy Wojciech Schabowy kto prowadzil miedzy innymi Arke i Cracovie?","??????"));
+        quiz.add(new QuizForm("Savoir-Vivre","Ja to sie nie wpierdalam czy nie wpraszam? Jak swoja szkoleniowa taktyke opisywal Pawel Janas","?????"));
+        quiz.add(new QuizForm("Jezyk Polski","Franciszek Smuda to trener skuteczny. Zapowiedzial walke o spadek i rzeczywiscie spadl. Z jakim klubem?","?????"));
     }
 
     //udostepniamy liste wszytskich quizow
     @GetMapping("/home")
-    public  List<Quiz> quizList() {
-        return quiz;
+    public ModelAndView quizList() {
+       ModelAndView mvn = new ModelAndView("home");
+       mvn.addObject("quizList", quiz);
+       return mvn;
     }
 
 
--- src/main/java/com/example/customlogin/controller/LoginController.java
+++ src/main/java/com/example/customlogin/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.example.customlogin.controller;
 
 
+import com.example.customlogin.form.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login () {
-        return "login";
+    public ModelAndView login() {
+        ModelAndView mvn = new ModelAndView("login");
+        mvn.addObject("login", new LoginForm());
+        return mvn;
     }
 }
